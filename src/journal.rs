da792d0ec2324dc51409a7ec1d434081646ccc8c
Paul Asmuth <paul@asmuth.com>
2018-06-01 17:53:18 +0200

    fix broken journal reader, remove unused functionality

--- src/journal.rs
+++ src/journal.rs
@@ -137,29 +137,11 @@ pub enum JournalFiles {
 /// Seeking position in journal.
 pub enum JournalSeek {
 	Head,
-	Current,
 	Tail,
-	ClockMonotonic {
-		boot_id: Id128,
-		usec: u64,
-	},
-	ClockRealtime {
-		usec: u64,
-	},
-	Cursor {
-		cursor: String,
-	},
 }
-
-#[derive(Clone, Debug)]
-pub enum JournalWaitResult {
-	Nop,
-	Append,
-	Invalidate
-}
-
 
 impl Journal {
+
 	/// Open the systemd journal for reading.
 	///
 	/// Params:
@@ -188,7 +170,7 @@ impl Journal {
 
 		let mut journal = Journal { j: ptr::null_mut() };
 		sd_try!(ffi::sd_journal_open(&mut journal.j, flags));
-		sd_try!(ffi::sd_journal_seek_head(journal.j));
+
 		Ok(journal)
 	}
 
@@ -223,8 +205,7 @@ impl Journal {
 			return Ok(None);
 		}
 
-		self.get_record()
-		
+		return self.get_record();
 	}
 
 	/// Read the previous record from the journal. Returns `Ok(None)` if there
@@ -234,138 +215,24 @@ impl Journal {
 			return Ok(None);
 		}
 		
-		self.get_record()
+		return self.get_record();
 	}
-
-	/// Wait for next record to arrive.
-	/// Pass wait_time `None` to wait for an unlimited period for new records.
-	fn wait(&mut self, wait_time: Option<time::Duration>) -> Result<JournalWaitResult> {
-
-		let time = wait_time.map(usec_from_duration).unwrap_or(::std::u64::MAX);
-
-		match sd_try!(ffi::sd_journal_wait(self.j, time)) {
-			ffi::SD_JOURNAL_NOP => Ok(JournalWaitResult::Nop),
-			ffi::SD_JOURNAL_APPEND => Ok(JournalWaitResult::Append),
-			ffi::SD_JOURNAL_INVALIDATE => Ok(JournalWaitResult::Invalidate),
-			_ => Err(io::Error::new(InvalidData, "Failed to wait for changes"))
-		}
-	}
-
-	/// Wait for the next record to appear. Returns `Ok(None)` if there were no
-	/// new records in the given wait time.
-	/// Pass wait_time `None` to wait for an unlimited period for new records.
-	pub fn await_next_record(&mut self, wait_time: Option<time::Duration>) -> Result<Option<JournalRecord>> {
-		match self.wait(wait_time)? {
-			JournalWaitResult::Nop => Ok(None),
-			JournalWaitResult::Append => self.next_record(),
-
-			// This is possibly wrong, but I can't generate a scenario with 
-			// ..::Invalidate and neither systemd's journalctl, 
-			// systemd-journal-upload, and other utilities handle that case.
-			JournalWaitResult::Invalidate => self.next_record()
-		}
-	}
-
-	/// Iterate through all elements from the current cursor, then await the
-	/// next record(s) and wait again.
-	pub fn watch_all_elements<F>(&mut self, mut f: F) -> Result<()>
-		where F: FnMut(JournalRecord) -> Result<()> {
-			loop {
-				let candidate = self.next_record()?;
-				let rec = match candidate {
-					Some(rec) => rec,
-					None => { loop {
-						if let Some(r) = self.await_next_record(None)? {
-							break r;
-						}
-					}}
-				};
-				f(rec)?
-			}
-		}
 
 	/// Seek to a specific position in journal. On success, returns a cursor
 	/// to the current entry.
-	pub fn seek(&mut self, seek: JournalSeek) -> Result<String> {
+	pub fn seek(&mut self, seek: JournalSeek) -> Result<()> {
 		match seek {
 			JournalSeek::Head => sd_try!(ffi::sd_journal_seek_head(self.j)),
-			JournalSeek::Current => 0,
 			JournalSeek::Tail => sd_try!(ffi::sd_journal_seek_tail(self.j)),
-			JournalSeek::ClockMonotonic { boot_id, usec } => {
-				sd_try!(ffi::sd_journal_seek_monotonic_usec(self.j,
-															sd_id128_t {
-																bytes: *boot_id.as_bytes(),
-															},
-															usec))
-			}
-			JournalSeek::ClockRealtime { usec } => {
-				sd_try!(ffi::sd_journal_seek_realtime_usec(self.j, usec))
-			}
-			JournalSeek::Cursor { cursor } => {
-				let c = try!(CString::new(cursor));
-				sd_try!(ffi::sd_journal_seek_cursor(self.j, c.as_ptr()))
-			}
 		};
-		let c: *mut c_char = ptr::null_mut();
-		if unsafe { ffi::sd_journal_get_cursor(self.j, &c) != 0 } {
-			// Cursor may need to be re-aligned on a real entry first.
-			sd_try!(ffi::sd_journal_next(self.j));
-			sd_try!(ffi::sd_journal_get_cursor(self.j, &c));
-		}
-		let cs = free_cstring(c).unwrap();
-		Ok(cs)
+
+		return Ok(());
 	}
 
-	/// Returns the cursor of current journal entry
-	pub fn cursor(&self) -> Result<String> {
-		let mut c_cursor: *mut c_char = ptr::null_mut();
-
-		sd_try!(ffi::sd_journal_get_cursor(self.j, &mut c_cursor));
-		let cursor = free_cstring(c_cursor).unwrap();
-		Ok(cursor)
-	}
-
-	/// Returns timestamp at which current journal is recorded
-	pub fn timestamp(&self) -> Result<time::SystemTime> {
-		let mut timestamp_us: u64 = 0;
-		sd_try!(ffi::sd_journal_get_realtime_usec(self.j, &mut timestamp_us));
-		Ok(system_time_from_realtime_usec(timestamp_us))
-	}
-
-	/// Adds a match by which to filter the entries of the journal.
-	/// If a match is applied, only entries with this field set will be iterated.
-	pub fn match_add<T: Into<Vec<u8>>>(&mut self, key: &str, val: T) -> Result<&mut Journal> {
-		let mut filter = Vec::<u8>::from(key);
-		filter.push('=' as u8);
-		filter.extend(val.into());
-		let data = filter.as_ptr() as *const c_void;
-		let datalen = filter.len() as size_t;
-		sd_try!(ffi::sd_journal_add_match(self.j, data, datalen));
-		Ok(self)
-	}
-
-	/// Inserts a disjunction (i.e. logical OR) in the match list.
-	pub fn match_or(&mut self) -> Result<&mut Journal> {
-		sd_try!(ffi::sd_journal_add_disjunction(self.j));
-		Ok(self)
-	}
-
-	/// Inserts a conjunction (i.e. logical AND) in the match list.
-	pub fn match_and(&mut self) -> Result<&mut Journal> {
-		sd_try!(ffi::sd_journal_add_conjunction(self.j));
-		Ok(self)
-	}
-
-	/// Flushes all matches, disjunction and conjunction terms.
-	/// After this call all filtering is removed and all entries in
-	/// the journal will be iterated again.
-	pub fn match_flush(&mut self) -> Result<&mut Journal> {
-		unsafe { ffi::sd_journal_flush_matches(self.j) };
-		Ok(self)
-	}
 }
 
 impl Drop for Journal {
+
 	fn drop(&mut self) {
 		if !self.j.is_null() {
 			unsafe {
@@ -373,4 +240,5 @@ impl Drop for Journal {
 			}
 		}
 	}
+
 }
--- tests/journal.rs
+++ tests/journal.rs
@@ -6,106 +6,36 @@ extern crate log;
 use std::path::Path;
 use systemd::journal;
 
-// Some systems don't have a running journal, which causes our tests to fail currently
-//
-// TODO: adjust tests that use this to generate a fixed journal if possible, or ship some test
-// data.
-fn have_journal() -> bool {
-    if !Path::new("/run/systemd/journal/").exists() {
-        println!("missing journal files");
-        false
-    } else {
-        true
-    }
-}
-
 #[test]
-fn test() {
-    journal::send(&["CODE_FILE=HI", "CODE_LINE=1213", "CODE_FUNCTION=LIES"]);
-    journal::print(1, &format!("Rust can talk to the journal: {}", 4));
+fn test_reverse_walk() {
+	let messages_expected = vec![
+		"rust-systemd test 1",
+		"rust-systemd test 2",
+		"rust-systemd test 3",
+	];
 
-    journal::JournalLog::init().ok().unwrap();
-    log::set_max_level(log::LevelFilter::Warn);
-    log!(log::Level::Info, "HI info");
-    log!(target: "systemd-tests", log::Level::Info, "HI info with target");
-    log!(log::Level::Warn, "HI warn");
-    log!(target: "systemd-tests", log::Level::Warn, "HI warn with target");
-    sd_journal_log!(4, "HI {:?}", 2);
+	for message in &messages_expected {
+		journal::send(&[&format!("MESSAGE={}", message)]);
+	}
+
+	let mut journal = journal::Journal
+			::open(journal::JournalFiles::All, false, false)
+			.expect("journal open failed");
+
+	journal
+			.seek(journal::JournalSeek::Tail)
+			.expect("journal seek failed");
+
+	let mut messages_actual = Vec::<String>::new();
+
+	for _ in 0..3 {
+		let entry = journal
+				.previous_record()
+				.expect("previous_record() failed");
+
+		messages_actual.insert(0, entry.unwrap().get("MESSAGE").unwrap().to_string());
+	}
+
+	assert!(messages_expected == messages_actual);
 }
 
-#[test]
-fn cursor() {
-    if ! have_journal() {
-        return;
-    }
-
-    let mut j = journal::Journal::open(journal::JournalFiles::All, false, false).unwrap();
-    log!(log::Level::Info, "rust-systemd test_seek entry");
-    assert!(j.seek(journal::JournalSeek::Head).is_ok());
-    let _s = j.cursor().unwrap();
-}
-
-#[test]
-fn ts() {
-    if ! have_journal() {
-        return;
-    }
-
-    let mut j = journal::Journal::open(journal::JournalFiles::All, false, false).unwrap();
-    log!(log::Level::Info, "rust-systemd test_seek entry");
-    assert!(j.seek(journal::JournalSeek::Head).is_ok());
-    let _s = j.timestamp().unwrap();
-}
-
-
-#[test]
-fn test_seek() {
-    let mut j = journal::Journal::open(journal::JournalFiles::All, false, false).unwrap();
-    if ! have_journal() {
-        return;
-    }
-    log!(log::Level::Info, "rust-systemd test_seek entry");
-    assert!(j.seek(journal::JournalSeek::Head).is_ok());
-    assert!(j.next_record().is_ok());
-    let c1 = j.seek(journal::JournalSeek::Current);
-    assert!(c1.is_ok());
-    let c2 = j.seek(journal::JournalSeek::Current);
-    assert!(c2.is_ok());
-    assert_eq!(c1.unwrap(), c2.unwrap());
-    assert!(j.seek(journal::JournalSeek::Tail).is_ok());
-    assert!(j.next_record().is_ok());
-    let c3 = j.cursor().unwrap();
-    let valid_cursor = journal::JournalSeek::Cursor { cursor: c3 };
-    assert!(j.seek(valid_cursor).is_ok());
-    let invalid_cursor = journal::JournalSeek::Cursor { cursor: "".to_string() };
-    assert!(j.seek(invalid_cursor).is_err());
-}
-
-#[test]
-fn test_simple_match() {
-    if ! have_journal() {
-        return;
-    }
-    let key = "RUST_TEST_MARKER";
-    let value = "RUST_SYSTEMD_SIMPLE_MATCH";
-    let msg = "MESSAGE=rust-systemd test_match";
-    let filter = format!("{}={}", key, value);
-    let mut j = journal::Journal::open(journal::JournalFiles::All, false, false).unwrap();
-
-    // check for positive matches
-    assert!(j.seek(journal::JournalSeek::Tail).is_ok());
-    journal::send(&[&filter, &msg]);
-    assert!(j.match_flush().unwrap().match_add(key, value).is_ok());
-    let r = j.next_record().unwrap();
-    assert!(r.is_some());
-    let entry = r.unwrap();
-    let entryval = entry.get(key);
-    assert!(entryval.is_some());
-    assert_eq!(entryval.unwrap(), value);
-
-    // check for negative matches
-    assert!(j.seek(journal::JournalSeek::Tail).is_ok());
-    assert!(j.match_flush().unwrap().match_add("NOKEY", "NOVALUE").is_ok());
-    journal::send(&[&msg]);
-    assert!(j.next_record().unwrap().is_none());
-}
