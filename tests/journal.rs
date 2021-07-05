9756468f48235dd4aaa3f6763bdb29b9bb779f4c
Finn Behrens <fin@nyantec.com>
2021-07-05 12:06:05 +0200

    allow integration test to receive messages not sent by the test case.

--- tests/journal.rs
+++ tests/journal.rs
@@ -28,24 +28,47 @@ fn test_reverse_walk() {
 	let mut journal =
 		JournalReader::open(&JournalReaderConfig::default()).expect("journal open failed");
 
+	// give systemd internals some time
+	std::thread::sleep(std::time::Duration::from_micros(1000));
+
 	journal
 		.seek(JournalSeek::Tail)
 		.expect("journal seek failed");
 
-	for i in 1..(messages_expected.len() + 1) {
+	println!("time: {}", now_usec);
+
+	let mut i = 1;
+	let mut j = 1;
+	loop {
+		j += 1;
 		let entry = journal
 			.previous_entry()
 			.expect("previous_record() failed")
 			.unwrap();
 
 		let entry_message = entry.get_message().unwrap().to_string();
+		if !entry_message.starts_with("rust-systemd") {
+			j += 1;
+			// other logs should not create as many logs
+			assert!(j < 20);
+			continue;
+		}
+
+		let entry_time = entry.get_wallclock_time().unwrap().timestamp_us / 1000000;
+
+		println!("{}: {}", entry_message, entry_time);
+
 		assert_eq!(
 			entry_message,
 			messages_expected[messages_expected.len() - i]
 		);
 
 		let entry_time = entry.get_wallclock_time().unwrap().timestamp_us / 1000000;
+		assert!((entry_time - now_usec).abs() < TIME_EPSILON_SECONDS);
 
-		assert!((entry_time - now_usec).abs() < TIME_EPSILON_SECONDS)
+		i += 1;
+		if i == messages_expected.len() + 1 {
+			return;
+		}
 	}
 }
