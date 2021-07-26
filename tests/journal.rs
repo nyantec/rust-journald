ce645fb7e8732372ea7f9c80da2c01ebac5a284b
Finn Behrens <fin@nyantec.com>
2021-07-26 14:07:39 +0200

    base reverse_walk test on a journald filter
    Signed-off-by: Finn Behrens <fin@nyantec.com>

--- tests/journal.rs
+++ tests/journal.rs
@@ -6,8 +6,12 @@ use journald::JournalEntry;
 
 const TIME_EPSILON_SECONDS: i64 = 5;
 
+const FILTER_FIELD: &str = "RUST_JOURNALD_TEST";
+
 #[test]
 fn test_reverse_walk() {
+	let filter: String = format!("test_reverse_walk_{}", rand::random::<u64>());
+
 	let now_usec: i64 = SystemTime::now()
 		.duration_since(UNIX_EPOCH)
 		.unwrap()
@@ -22,54 +26,41 @@ fn test_reverse_walk() {
 	for message in &messages_expected {
 		let mut entry = JournalEntry::new();
 		entry.set_message(message);
+		entry
+			.fields
+			.insert(FILTER_FIELD.to_string(), filter.to_string());
 		journald::writer::submit(&entry).expect("journald write failed");
 	}
 
 	let mut journal =
 		JournalReader::open(&JournalReaderConfig::default()).expect("journal open failed");
 
+	journal
+		.add_filter(&format!("{}={}", FILTER_FIELD, filter))
+		.expect("Could not set journald filter");
+
 	// give systemd internals some time
-	std::thread::sleep(std::time::Duration::from_micros(1000));
+	std::thread::sleep(std::time::Duration::from_secs(1));
 
 	journal
 		.seek(JournalSeek::Tail)
 		.expect("journal seek failed");
 
-	println!("time: {}", now_usec);
-
-	let mut i = 1;
-	let mut j = 1;
-	loop {
-		j += 1;
+	for i in 1..(messages_expected.len() + 1) {
 		let entry = journal
 			.previous_entry()
-			.expect("previous_record() failed")
-			.unwrap();
+			.expect("previous_entry failed")
+			.expect("No entry found");
 
-		let entry_message = entry.get_message().unwrap().to_string();
-		if !entry_message.starts_with("rust-systemd") {
-			j += 1;
-			// other logs should not create as many logs
-			assert!(j < 20);
-			continue;
-		}
-
-		let entry_time = entry.get_wallclock_time().unwrap().timestamp_us / 1000000;
-
-		println!("{}: {}", entry_message, entry_time);
-
+		let entry_message = entry.get_message().expect("No message").to_string();
 		assert_eq!(
 			entry_message,
 			messages_expected[messages_expected.len() - i]
 		);
 
 		let entry_time = entry.get_wallclock_time().unwrap().timestamp_us / 1000000;
-		assert!((entry_time - now_usec).abs() < TIME_EPSILON_SECONDS);
 
-		i += 1;
-		if i == messages_expected.len() + 1 {
-			return;
-		}
+		assert!((entry_time - now_usec).abs() < TIME_EPSILON_SECONDS)
 	}
 }
 
