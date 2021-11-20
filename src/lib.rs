24c96949cac7356aa554f4b94ace7f4328eb492c
Finn Behrens <fin@nyantec.com>
2021-11-20 12:07:17 +0100

    add dlopen functionality
    Signed-off-by: Finn Behrens <fin@nyantec.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,28 @@
+use std::env::var;
 pub use std::io::{Error, Result};
+use std::sync::atomic::Ordering::SeqCst;
+use std::sync::atomic::{AtomicBool, AtomicUsize};
 
+#[cfg(feature = "open")]
+use dlopen::wrapper::{Container, WrapperApi};
+use libc::{c_char, c_int, c_void, size_t};
+#[cfg(feature = "open")]
+#[macro_use]
+extern crate dlopen_derive;
+
+#[cfg(feature = "libsystemd-sys")]
 use libsystemd_sys as ffi;
 
+#[path = "journal_entry.rs"]
+mod entry;
+pub use self::entry::*;
+
+pub mod reader;
+
+#[path = "journal_writer.rs"]
+pub mod writer;
 /// Convert a systemd ffi return value into a Result
-pub fn ffi_result(ret: ffi::c_int) -> Result<ffi::c_int> {
+pub fn ffi_result(ret: c_int) -> Result<c_int> {
 	if ret < 0 {
 		Err(Error::from_raw_os_error(-ret))
 	} else {
@@ -26,11 +45,92 @@ macro_rules! sd_try {
 	}};
 }
 
-#[path = "journal_entry.rs"]
-mod entry;
-pub use self::entry::*;
+#[cfg(feature = "open")]
+pub(crate) static mut SYSTEMD_API: Option<Container<SystemdApi>> = None;
+#[cfg(feature = "open")]
+static SYSTEMD_API_LOADING: AtomicUsize = AtomicUsize::new(0);
 
-pub mod reader;
+#[derive(WrapperApi)]
+#[cfg(feature = "open")]
+pub(crate) struct SystemdApi {
+	sd_journal_open: unsafe extern "C" fn(sd_journal: *mut *mut c_void, flags: c_int) -> c_int,
+	sd_journal_open_namespace: unsafe extern "C" fn(
+		sd_journal: *mut *mut c_void,
+		name: *const c_char,
+		flags: c_int,
+	) -> c_int,
 
-#[path = "journal_writer.rs"]
-pub mod writer;
+	sd_journal_restart_data: unsafe extern "C" fn(sd_journal: *mut c_void),
+	sd_journal_enumerate_data: unsafe extern "C" fn(
+		sd_journal: *mut c_void,
+		data: *mut *const u8,
+		length: *mut size_t,
+	) -> c_int,
+	sd_journal_get_realtime_usec:
+		unsafe extern "C" fn(sd_journal: *mut c_void, usec: *mut u64) -> c_int,
+	sd_journal_get_monotonic_usec: unsafe extern "C" fn(
+		sd_journal: *mut c_void,
+		usec: *mut u64,
+		boot_id: *mut c_void,
+	) -> c_int,
+
+	sd_journal_next: unsafe extern "C" fn(sd_journal: *mut c_void) -> c_int,
+	sd_journal_previous: unsafe extern "C" fn(sd_journal: *mut c_void) -> c_int,
+
+	sd_journal_seek_head: unsafe extern "C" fn(sd_journal: *mut c_void) -> c_int,
+	sd_journal_seek_tail: unsafe extern "C" fn(sd_journal: *mut c_void) -> c_int,
+	sd_journal_seek_cursor:
+		unsafe extern "C" fn(sd_journal: *mut c_void, coursor: *const c_char) -> c_int,
+	sd_journal_get_cursor:
+		unsafe extern "C" fn(sd_journal: *mut c_void, coursor: *mut *const c_char) -> c_int,
+
+	sd_journal_wait: unsafe extern "C" fn(sd_journal: *mut c_void, usec: u64) -> c_int,
+
+	sd_journal_add_match:
+		unsafe extern "C" fn(sd_journal: *mut c_void, data: *mut c_void, size: size_t) -> c_int,
+
+	sd_journal_sendv: unsafe extern "C" fn(iv: *const writer::const_iovec, n: c_int) -> c_int,
+
+	sd_journal_close: unsafe extern "C" fn(sd_journal: *mut c_void),
+}
+
+#[cfg(feature = "open")]
+pub(crate) fn open_systemd<'a>() -> Result<&'a mut Container<SystemdApi>> {
+	let loading =
+		SYSTEMD_API_LOADING.fetch_update(SeqCst, SeqCst, |x| if x == 0 { Some(1) } else { None });
+	match loading {
+		Err(1) => return Err(Error::from_raw_os_error(libc::EAGAIN)),
+		Err(2) => return Err(Error::from_raw_os_error(libc::ENOSYS)),
+		Err(4) => return Ok(unsafe { SYSTEMD_API.as_mut() }.unwrap()),
+		Ok(0) => (),
+		_ => return Err(Error::from_raw_os_error(libc::ENOTRECOVERABLE)),
+	}
+
+	let api: std::result::Result<Container<SystemdApi>, dlopen::Error> =
+		unsafe { Container::load("libsystemd.so.0") };
+	let api = match api {
+		Err(dlopen::Error::OpeningLibraryError(x)) => Err(x),
+		Err(dlopen::Error::SymbolGettingError(x)) => Err(x),
+		Err(_x) => Err(Error::from_raw_os_error(libc::ENOTRECOVERABLE)),
+		Ok(v) => Ok(v),
+	};
+
+	if let Err(x) = api {
+		SYSTEMD_API_LOADING.store(2, SeqCst);
+		return Err(x);
+	}
+
+	let mut api = api.unwrap();
+	unsafe { SYSTEMD_API = Some(api) };
+	SYSTEMD_API_LOADING.store(4, SeqCst);
+	Ok(unsafe { SYSTEMD_API.as_mut() }.unwrap())
+}
+
+#[cfg(test)]
+mod test {
+	#[cfg(feature = "open")]
+	#[test]
+	fn open_api() {
+		let api = super::open_systemd().unwrap();
+	}
+}
